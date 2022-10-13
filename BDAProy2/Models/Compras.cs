51366a4097431016784692619a904c4639ea6de2
gabogh123 <gabogh99@gmail.com>
2022-10-12 22:36:10 -0600

    feat(Compras): Create model

---
+++ BDAProy2/Models/Compras.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BDAProy2.Models
+{
+    public class Compras
+    {
+        public int idCliente { get; set; }
+        public int idProducto { get; set; }
+        public int cantidad { get; set; }
+    }
+}
