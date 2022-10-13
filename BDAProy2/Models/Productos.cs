5c99a614244396841219ee116833b44c65fc6079
gabogh123 <gabogh99@gmail.com>
2022-10-12 23:58:52 -0600

    feat(Productos): add model parameters

--- BDAProy2/Models/Productos.cs
+++ BDAProy2/Models/Productos.cs
@@ -7,5 +7,9 @@ namespace BDAProy2.Models
 {
     public class Productos
     {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string marca { get; set; }
+        public int precio { get; set; }
     }
 }
