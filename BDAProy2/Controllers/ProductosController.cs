71fb26cd43d1698ac506332dd15a1822b6130528
gabogh123 <gabogh99@gmail.com>
2022-10-13 00:54:25 -0600

    feat(Productos): Add CRUD methods

---
+++ BDAProy2/Controllers/ProductosController.cs
@@ -0,0 +1,76 @@
+﻿using BDAProy2.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Neo4jClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BDAProy2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductosController : ControllerBase
+    {
+
+        private readonly IGraphClient _client;
+
+        public ProductosController(IGraphClient client)
+        {
+            _client = client;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var productos = await _client.Cypher.Match("(x:Productos)")
+                .Return(x => x.As<Productos>()).ResultsAsync;
+
+            return Ok(productos);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var productos = await _client.Cypher.Match("(x:Productos)")
+                                               .Where((Productos x) => x.id == id)
+                                               .Return(x => x.As<Productos>()).ResultsAsync;
+            return Ok(productos.LastOrDefault());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] Productos prod)
+        {
+            await _client.Cypher.Create("(x:Productos $prod)")
+                                .WithParam("prod", prod)
+                                .ExecuteWithoutResultsAsync();
+
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] Productos prod)
+        {
+            await _client.Cypher.Match("(x:Productos)")
+                                .Where((Productos x) => x.id == id)
+                                .Set("x = $prod")
+                                .WithParam("prod", prod)
+                                .ExecuteWithoutResultsAsync();
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _client.Cypher.Match("(x:Productos)")
+                                .Where((Productos x) => x.id == id)
+                                .Delete("x")
+                                .ExecuteWithoutResultsAsync();
+
+            return Ok();
+        }
+
+    }
+}
